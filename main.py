6a99f8539a25b71a00f95acc8a1e7902f31dfd7a
AntonovArtur <eichholtz@yandex.ru>
2023-05-28 17:39:48 +0700

    remove types from module telebot

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import os
 
 import telebot
-from telebot import types
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager

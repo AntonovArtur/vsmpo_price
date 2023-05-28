33bdba1864f865063ac6920ff2009916b3b475f1
AntonovArtur <eichholtz@yandex.ru>
2023-05-28 14:24:49 +0700

    added schedule for sending message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+import schedule as schedule
 import telebot
 from telebot import types
 from selenium import webdriver
@@ -6,43 +8,80 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-# Опции для запуска браузера в режиме headless
-chrome_options = Options()
-chrome_options.add_argument("--headless")
 
-# Инициализация драйвера с помощью WebDriver Manager и опций headless
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+def get_price():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    url = 'https://www.moex.com/ru/issue.aspx?board=TQBR&code=VSMO&utm_source=www.moex.com&utm_term=vsmpo'
+    driver.get(url)
 
-# Переход на страницу
-url = 'https://www.moex.com/ru/issue.aspx?board=TQBR&code=VSMO&utm_source=www.moex.com&utm_term=vsmpo'
-driver.get(url)
+    # Поиск элемента на странице
+    driver.find_element(By.XPATH, "//div[@id='disclaimer-modal']//a[.='Согласен']").click()
+    element = driver.find_element(By.XPATH, "//div[@class='em_right_top']/span[@class='price']")
+    text_price = element.text
+    # Вывод текста элемента
+    # print(text_price)
 
-# Поиск элемента на странице
-driver.find_element(By.XPATH, "//div[@id='disclaimer-modal']//a[.='Согласен']").click()
-element = driver.find_element(By.XPATH, "//div[@class='em_right_top']/span[@class='price']")
-text_price = element.text
-# Вывод текста элемента
-print(text_price)
-
-# Закрытие браузера
-driver.quit()
-
-# Вставьте токен вашего бота Telegram
-bot_token = os.environ['BOT_TOKEN']
-supergroup_username = '@+RKBDwebcUqxjMDZi'
-# Вставьте ID группы, в которую вы хотите отправить сообщение +RKBDwebcUqxjMDZi
-channel_id = -1001960945097
-
-# Создаем экземпляр бота
-bot = telebot.TeleBot(bot_token)
+    # Закрытие браузера
+    driver.quit()
+    return text_price
 
 
-# Отправляем сообщение в канал
-def send_message_to_channel(message):
-    bot.send_message(channel_id, message, parse_mode='MarkdownV2')
+def send_price_to_chat(get_price_text):
+    # Вставьте токен вашего бота Telegram
+    bot_token = os.environ['BOT_TOKEN']
+    supergroup_username = '@+RKBDwebcUqxjMDZi'
+    # Вставьте ID группы, в которую вы хотите отправить сообщение +RKBDwebcUqxjMDZi
+    channel_id = -1001960945097
+
+    # Создаем экземпляр бота
+    bot = telebot.TeleBot(bot_token)
+
+    # Отправляем сообщение в канал
+    def send_message_to_channel(message):
+        bot.send_message(channel_id, message, parse_mode='MarkdownV2')
+
+    # Пример использования
+    text = "\n\n📈 Стоимость 1 акции ВСМПО\\-Ависма \(VSMO\) на московской бирже составляет *text_price*\n\\#мосбиржа \\#vsmpo"
+    text = text.replace("text_price", get_price_text)
+    send_message_to_channel(text)
 
 
-# Пример использования
-text = "\n\n📈 Стоимость 1 акции ВСМПО\\-Ависма \(VSMO\) на московской бирже составляет *text_price*\n\\#мосбиржа \\#vsmpo"
-text = text.replace("text_price", text_price)
-send_message_to_channel(text)
+# schedule.every().monday.to.friday.at('10:00').do(send_price_to_chat(get_price()))
+# while True:
+#     schedule.run_pending()
+
+import datetime
+import schedule
+import time
+import pytz
+
+
+def run_scheduled_method():
+    # Установка временной зоны
+    moscow_timezone = pytz.timezone('Europe/Moscow')
+
+    # Задание расписания выполнения метода по будням в 10:00
+    # schedule.every().day.at('09:59').do(print("jshdfkjhl"))
+
+    while True:
+        # Получение текущего времени в указанной временной зоне
+        current_time = datetime.datetime.now(moscow_timezone)
+
+        # Получение текущего дня недели
+        current_day = current_time.weekday()
+        print(current_time)
+        # Проверка расписания и выполнение заданных методов только по будням
+        # if current_day >= 0 and current_day <= 4 and current_time.hour == 9 and current_time.minute == 56 and current_time.second == 00:
+        if current_time.hour == 10 and current_time.minute == 23 and current_time.second == 0:
+            schedule.run_pending()
+            send_price_to_chat(get_price())
+
+        # Задержка в 1 секунду перед проверкой расписания снова
+        time.sleep(1)
+
+
+# Вызов функции для запуска выполнения метода по расписанию
+run_scheduled_method()
+

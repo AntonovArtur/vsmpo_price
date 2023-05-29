6bbbd6c774bd946e1249c22c205d36ea839848f8
AntonovArtur <eichholtz@yandex.ru>
2023-05-29 15:34:24 +0700

    11:40

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def run_scheduled_method():
 
         # Проверка расписания и выполнение заданных методов только по будням if current_day >= 0 and current_day <= 4
         #  and current_time.hour == 9 and current_time.minute == 56 and current_time.second == 00:
-        if current_day <= 4 and current_time.hour == 11 and current_time.minute == 30 and current_time.second == 0:
+        if current_day <= 4 and current_time.hour == 11 and current_time.minute == 40 and current_time.second == 0:
             schedule.run_pending()
             send_price_to_chat(get_price())
 
